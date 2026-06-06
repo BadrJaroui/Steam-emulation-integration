943c08d69d4b0cbfbcdd2224f55573f2efbdb5e8
BadrJaroui <bjaroui@gmail.com>
2026-06-06 23:08:02 +0200

    changed to checking for folder name rather than rom extension

--- utils.py
+++ utils.py
@@ -34,6 +34,20 @@ def parse_game_name(file_name):
         return file_name.split("[")[0].strip()
     if "." in file_name:
         return file_name.split(".")[0].strip()
+    
+def normalize_string(x):
+    return x.replace(" ", "")\
+            .replace("-", "")\
+            .replace("_", "")\
+            .lower()
+
+def retrieve_directory_name(file_path):
+    if "\\" in file_path:
+        return file_path.lower().split("\\roms")[1].split("\\")[1]
+    if "/" in file_path:
+        return file_path.lower().split("/roms")[1].split("/")[1]
+
+    return ValueError(f"Invalid path: {file_path}")
 
 def readData():
     with open("C:/Program Files (x86)/Steam/userdata/410602222/config/shortcuts.vdf", "rb") as f:
